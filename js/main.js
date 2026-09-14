15abb711b21c5749915643fee792d64770a07917
Shifat <yeasinkabirshifat@gmail.com>
2026-09-14 12:56:22 +0600

    checkout er delete svg changes

--- js/main.js
+++ js/main.js
@@ -94,7 +94,15 @@ function renderCart() {
           <button class="cart__qty-btn" onclick="changeQty('${item.id}',1)">+</button>
         </div>
       </div>
-      <button class="cart__delete" onclick="removeFromCart('${item.id}')">🗑</button>
+      <button class="cart__delete" onclick="removeFromCart('${item.id}')" aria-label="Remove item">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round">
+          <polyline points="3 6 5 6 21 6"/>
+          <path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6"/>
+          <path d="M10 11v6"/>
+          <path d="M14 11v6"/>
+          <path d="M9 6V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v2"/>
+        </svg>
+      </button>
     </div>
   `).join('');
   const totalItems = cart.reduce((s, i) => s + i.qty, 0);
